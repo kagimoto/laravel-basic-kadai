23822e66cab752b92c07da66f6052648bc9bb289
鍵本 大地 <kagimoto@idslan.local>
2024-05-28 16:45:36 +0900

    フォームとバリデーション課題

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Post;
+use App\Http\Requests\PostStoreRequest;
+
 class PostController extends Controller
 {
     public function index(){
@@ -18,5 +20,20 @@ class PostController extends Controller
         $post = Post::find($id);
 
         return view('posts.show',compact('post'));
+    }
+
+    public function create(){
+        return view('posts.create');
+    }
+
+    public function store(PostStoreRequest $request){
+        
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        //リダイレクト
+        return redirect("/posts/{$post->id}");
     }
 }
